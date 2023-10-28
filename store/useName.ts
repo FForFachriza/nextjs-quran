f1c6cdac3da204e3f3f1cf9458915a740cccca0c
FForFachriza <dimasavrian346@gmail.com>
2023-10-28 15:32:39 +0700

    feat: add ayat store

--- store/useName.ts
+++ store/useName.ts
@@ -4,6 +4,10 @@ import { persist, createJSONStorage } from "zustand/middleware";
 interface namestore {
   name: string;
   setName: (val: string) => void;
+  surat: string;
+  setSurat: (val: string) => void;
+  ayat: number;
+  setAyat: (val: number) => void;
 }
 // export const useName = create<namestore>((set) => ({
 //   name: "",
@@ -15,6 +19,10 @@ export const useNameStore = create<namestore>()(
     (set) => ({
       name: "",
       setName: (val) => set({ name: val }),
+      ayat: 0,
+      setAyat: (val) => set({ ayat: val }),
+      surat: "",
+      setSurat: (val) => set({ surat: val }),
     }),
     {
       name: "name-storage",
