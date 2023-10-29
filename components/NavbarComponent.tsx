36e4964db73e0ccb760dea982ab59bb51558a154
FForFachriza <dimasavrian346@gmail.com>
2023-10-30 01:46:18 +0700

    feat: repository accordion menu

--- components/NavbarComponent.tsx
+++ components/NavbarComponent.tsx
@@ -6,10 +6,11 @@ import { useSearchStore } from "@/store/useSearch";
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import Link from "next/link";
 
 export default function NavbarComponent() {
   const { setSearch } = useSearchStore.getState();
-  const { setTheme , theme} = useTheme();
+  const { setTheme, theme } = useTheme();
 
   return (
     <nav className="flex flex-row justify-between container p-4 items-center">
@@ -26,16 +27,47 @@ export default function NavbarComponent() {
                   <AccordionTrigger>Theme</AccordionTrigger>
                   <AccordionContent>
                     <section className="flex flex-col w-full items-start">
-                      <Button className="flex w-full justify-start my-2" variant={"outline"} onClick={() => setTheme("dark")}>
+                      <Button
+                        className="flex w-full justify-start my-2"
+                        variant={"outline"}
+                        onClick={() => setTheme("dark")}
+                      >
                         Dark Mode
                       </Button>
-                      <Button className="flex w-full justify-start my-2" variant={"outline"} onClick={() => setTheme("light")}>
+                      <Button
+                        className="flex w-full justify-start my-2"
+                        variant={"outline"}
+                        onClick={() => setTheme("light")}
+                      >
                         Light Mode
                       </Button>
-                      <Button className="flex w-full justify-start my-2" variant={"outline"} onClick={() => setTheme("system")}>
+                      <Button
+                        className="flex w-full justify-start my-2"
+                        variant={"outline"}
+                        onClick={() => setTheme("system")}
+                      >
                         System Mode
                       </Button>
                       <p className="text-center w-full my-2">Currently Active: {theme}</p>
+                    </section>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+              <Accordion type="single" collapsible>
+                <AccordionItem value="item-1">
+                  <AccordionTrigger>Repository</AccordionTrigger>
+                  <AccordionContent>
+                    <section className="flex flex-col w-full items-start">
+                      <Link className="w-full " href={"https://github.com/FForFachriza/nextjs-quran"}>
+                        <Button className="flex w-full justify-start my-2" variant={"outline"}>
+                          Github
+                        </Button>
+                      </Link>
+                      <Link className="w-full" href={"https://quran-api.santrikoding.com/"}>
+                        <Button className="flex w-full justify-start my-2" variant={"outline"}>
+                          API 
+                        </Button>
+                      </Link>
                     </section>
                   </AccordionContent>
                 </AccordionItem>
